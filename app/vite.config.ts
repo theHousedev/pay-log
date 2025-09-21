cf2c6684eef55b082876b67fdf5d472664603124
kh <kylemhouseholder@gmail.com>
2025-09-21 14:29:57 -0500

    FIX: updated vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,15 +5,22 @@ import fs from 'fs'
 import yaml from 'js-yaml'
 import react from '@vitejs/plugin-react'
 
+type PortCfg = {
+  ports: {
+    backend: string
+    frontend: string
+  }
+}
+
 function getConfig() {
   try {
     const configPath = '../cfg.yaml'
     const fileContents = fs.readFileSync(configPath, 'utf8')
-    const config = yaml.load(fileContents) as any
+    const config = yaml.load(fileContents) as PortCfg
     return config
   } catch (error) {
-    console.warn('Could not read cfg.yaml, B=5999, F=6999')
-    return { backend_port: '5999', frontend_port: '6999' }
+    console.warn('Could not read cfg.yaml, B=5999, F=6999! ', error)
+    return { ports: { backend: '5999', frontend: '6999' } }
   }
 }
 
@@ -21,7 +28,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: parseInt(getConfig().frontend_port),
+    port: parseInt(getConfig().ports.frontend),
     allowedHosts: ['pay.khouse.dev', 'localhost', '127.0.0.1'],
     hmr: false
   },
@@ -31,6 +38,6 @@ export default defineConfig({
     },
   },
   define: {
-    BACKEND_PORT: JSON.stringify(getConfig().backend_port),
+    BACKEND_PORT: JSON.stringify(getConfig().ports.backend),
   }
 })
